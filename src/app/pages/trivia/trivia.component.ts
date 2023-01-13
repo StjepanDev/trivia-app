bf6a2fb270a1302d5b5b42845ef16d32a2079e74
StjepanDev <stjepansuhic@yahoo.com>
2023-01-13 11:43:55 +0100

    fff

---
+++ src/app/pages/trivia/trivia.component.ts
@@ -0,0 +1,19 @@
+import { Component, OnInit } from '@angular/core';
+import { TriviaService } from 'src/app/services/trivia.service';
+
+@Component({
+  selector: 'app-trivia',
+  templateUrl: './trivia.component.html',
+  styleUrls: ['./trivia.component.css']
+})
+export class TriviaComponent implements OnInit {
+
+  constructor(public triviaService:TriviaService){}
+
+
+  ngOnInit(): void {
+    this.triviaService.getQuestions()
+
+  }
+}
+S
